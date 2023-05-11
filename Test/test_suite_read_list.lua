46263a6567060270f708fea0b359f5c3d727a7d5
Valdas <116576563+ValdasKa@users.noreply.github.com>
2023-05-11 04:38:30 +0000

    Fix unit test for read list

--- Test/test_suite_read_list.lua
+++ Test/test_suite_read_list.lua
@@ -3,20 +3,17 @@ package.path = package.path .. ";../?.lua"
 local lu = require("luaunit")
 require("speed_test")
 
-
 function TestReadList:setUp()
     self.fname = 'speedtest_server_list.json'
-    
-end
-function TestReadList:TestReadServerListFile()
-    self.fopen = io.open
-    function io.open(...) return false end
-    lu.assertErrorMsgEquals("../speed_test.lua:129: Error opening server list file for reading",ReadServerList)
-    io.open = self.fopen
     end
 
+function TestReadList:TestReadServerListFileOpen()
+    self.fopen = io.open
+    function io.open(...) return false end
+    lu.assertErrorMsgEquals("Error opening server list file for reading",ReadServerList)
+    io.open = self.fopen
+    end
 function TestReadList.TestReadServerListDecodeData()
     lu.assertEvalToTrue(ReadServerList)
 end
-
    return TestReadList
